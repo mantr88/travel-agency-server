896da2394902809f87825620c4a52f8561ae1446
AntonPetrenko <109439896+mantr88@users.noreply.github.com>
2024-04-10 02:41:08 +0300

    add: db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+import { DataSource } from 'typeorm';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { connect } from './connect-db';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), TypeOrmModule.forRoot({
+    ...connect,
+    type: 'postgres',
+    logging: true,
+    entities: ['dist/**/entities/*.entity.js'],
+    migrations: ['dist/db/migrations/*.js'],
+    synchronize: false,
+    namingStrategy: new SnakeNamingStrategy(),
+  }), UsersModule, AuthModule,],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { constructor(private dataSource: DataSource) { } }
